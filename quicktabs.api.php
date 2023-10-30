4733b27965fcbb80a6b31cd9790c4e7a8b146621
Herb v/d Dool <herb@3speedhub.com>
2023-10-30 12:20:20 -0400

    convert variable to config. Update API docs.

--- quicktabs.api.php
+++ quicktabs.api.php
@@ -3,10 +3,35 @@
 /**
  * Alter the Quicktabs instance before it gets rendered.
  *
- * @param &$quicktabs
- *   A loaded Quicktabs object, either from the database or from code.
+ * @param object &$quicktabs
+ *   A loaded Quicktabs object.
  */
 function hook_quicktabs_alter(&$quicktabs) {
+  // Add another tab.
+  $quicktabs->tabs[] = array(
+    'nid' => 1,
+    'view_mode' => 'full',
+    'hide_title' => 1,
+    'title' => 'My first tab',
+    'weight' => '-1000',
+    'type' => 'node',
+  );
+}
+
+/**
+ * Alter the tablinks attributes before they get rendered.
+ *
+ * @param array &$attributes
+ *   An array of tablink attributes.
+ * @param object &$quickset
+ *   The quickset that the tablink belongs to.
+ * @param string $key
+ *   The key of the set item.
+ *
+ */
+function hook_quicktabs_tablinks_attributes_alter(&$attributes, &$quickset, $key) {
+  // Add another class
+  $attributes['class'][] = 'my-custom-class';
 }
 
 /**
@@ -19,5 +44,15 @@ function hook_quicktabs_alter(&$quicktabs) {
  *   file for a quick tabs style. Each value should be the name of the style.
  */
 function hook_quicktabs_tabstyles() {
+  $tabstyles_directory = backdrop_get_path('module', 'my_tabstyles') . '/tabstyles';
+  $files = file_scan_directory($tabstyles_directory, '/\.css$/');
+  $tabstyles = array();
+  foreach ($files as $file) {
+    // Skip RTL files.
+    if (!strpos($file->name, '-rtl')) {
+      $tabstyles[$file->uri] = backdrop_ucfirst($file->name);
+    }
+  }
+  return $tabstyles;
 }
 
